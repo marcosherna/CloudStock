945ce08fd5cb4a2b71430e20798adeb68969a5db
marcosherna <marcosxdf6@gmail.com>
2025-04-09 14:29:52 -0600

    build: usign cors

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const errorHandler = require('./handlers/errorHandler');
+const cors = require('cors');
 
 module.exports = function Server({ apiRouter }) {  
     const api = express();  
     
     api.use(express.json());
     api.use(express.urlencoded({ extended: true }));
+
+    api.use(cors({
+        origin: '*', // Allow all origins
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+        // allowedHeaders: ['Content-Type', 'Authorization'], 
+    }));
+
     api.use(express.static('public'));
     api.use(express.static('uploads')); 
 
